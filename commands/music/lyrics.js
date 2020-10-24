affa7b8ad1eb514e4c08bc493497e91bac232c97
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-10-23 19:22:49 -0700

    Disabled lyrics command

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -3,6 +3,69 @@ const genius = require(`genius-lyrics-api`);
 const config = require(`config`);
 const geniusAPI = config.get(`Bot.GENIUS_API`);
 
+const determineLyrics = (lyrics, page) => {
+
+};
+
+const sendEmbed = async (message, lyrics, page) => {
+    return await message.channel.send(new MessageEmbed()
+        .setTitle(result.title)
+        .setURL(result.url)
+        .setThumbnail(result.albumArt)
+        .setDescription(determineLyrics(lyrics, page))
+        .setColor(`#2EC14E`));
+};
+
+const reactionHandler = async (message, page, sent) => {
+    const filter = (reaction, user) => {
+        return ['◀️', '▶️'].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+
+    if (page == 0 && !queue[5]) {
+
+        // Only first page exists
+
+    } else if (!queue[(page + 1) * 5]) {
+
+        // Last page
+        sent.react('◀️')
+            .catch(() => console.error('One of the emojis failed to react.'));
+
+    } else if (page == 0) {
+
+        // First page
+        sent.react('▶️');
+
+    } else if (page > 0) {
+
+        // Any page between first and last
+        sent.react('◀️')
+            .then(() => sent.react('▶️'))
+            .catch(() => console.error('One of the emojis failed to react.'));
+
+    }
+
+    sent.awaitReactions(filter, { max: 1, time: 300000, errors: ['time'] })
+        .then(async collected => {
+            const reaction = collected.first();
+
+            if (reaction.emoji.name === '◀️') {
+                // Previous page
+                sent.delete();
+                let newSent = await sendEmbed(page - 1, message);
+                reactionHandler(newSent, message, page - 1);
+            } else if (reaction.emoji.name === "▶️") {
+                // Next page
+                sent.delete();
+                let newSent = await sendEmbed(page + 1, message);
+                reactionHandler(newSent, message, page + 1);
+            }
+        })
+        .catch(async collected => {
+            sent.reactions.removeAll();
+        });
+};
+
 module.exports = {
     name: 'lyrics',
     description: 'Find the lyrics to a certain song',
@@ -12,13 +75,19 @@ module.exports = {
     altUsage: '"[song title]" by [song artist]',
     // cooldown: 5,
     // guildOnly: true,
-    enabled: true,
+    enabled: false,
     restrictions: {
         resolvable: [],
         id: [],
     },
     type: 'music',
     async execute(message, args) {
+
+        let page = 0;
+
+        let result;
+        let lyrics;
+
         let title;
         let artist;
 
@@ -50,25 +119,12 @@ module.exports = {
                 .setDescription(`<:cross:729019052571492434> No results found`)
                 .setColor(`#FF3838`));
 
-            let result = songs[0];
+            result = songs[0];
 
-            genius.getLyrics(options).then(async lyrics => {
-                try {
-                    await message.channel.send(new MessageEmbed()
-                        .setTitle(result.title)
-                        .setURL(result.url)
-                        .setThumbnail(result.albumArt)
-                        .setDescription(lyrics)
-                        .setColor(`#2EC14E`));
-                } catch (error) {
-                    if (error.code == 50035) {
-                        message.channel.send(result.url);
+            genius.getLyrics(options).then(resultLyrics => {
+                lyrics = resultLyrics;
 
-                        message.channel.send(new MessageEmbed()
-                            .setDescription(`<:cross:729019052571492434> Sorry, the lyrics were too long to fit in Discord's message limits. Use this link instead.`)
-                            .setColor(`#FF3838`));
-                    }
-                }
+                sendEmbed(message, lyrics, 1);
             });
         });
     }
